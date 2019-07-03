f515d15cb984dc3863943c1070a300a0f0589ed6
Emily DeWitt <emilykdewitt@gmail.com>
2019-07-02 20:34:52 -0500

    got links in navbar working

--- src/Components/EditScat/EditScat.js
+++ src/Components/EditScat/EditScat.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 class EditScat extends React.Component {
   render() {
+    const editId = this.props.match.params.id;
     return (
       <div className="EditScat">
         <h1>EditScat</h1>
+        <h2>The editId is {editId}</h2>
       </div>
     );
   }
