3bfa316a0b51a0fb329c5ecb3cbc8469a41fcec1
Emily DeWitt <emilykdewitt@gmail.com>
2019-07-02 19:36:41 -0500

    Added a bunch of component files

---
+++ src/Components/EditScat/EditScat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EditScat extends React.Component {
+  render() {
+    return (
+      <div className="EditScat">
+        <h1>EditScat</h1>
+      </div>
+    );
+  }
+}
+
+export default EditScat;
---
+++ src/Components/SingleScat/SingleScat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SingleScat extends React.Component {
+  render() {
+    return (
+      <div className="SingleScat">
+        <h1>SingleScat</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleScat;
